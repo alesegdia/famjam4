e4a182825f77228614bdd05368025b2b612ccaab
alesegdia <alesegdia@gmail.com>
2015-03-20 21:45:41 +0100

    Generalizing scripts.

---
+++ project/Assets/Scripts/LookAtController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtController : MonoBehaviour {
+
+    public GameObject pawn;
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    Vector3 delta = target.transform.position - pawn.transform.position;
+        float rot_z = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+        pawn.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+	}
+}
