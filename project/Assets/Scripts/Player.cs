519a86135d57af805674a5dec6db20baf0e0e450
Daniel <danmarqui@gmail.com>
2015-03-21 00:23:25 +0100

    Subo algo mas de la parte de conversación

--- project/Assets/Scripts/Player.cs
+++ project/Assets/Scripts/Player.cs
@@ -5,4 +5,5 @@ public class Player : MonoBehaviour {
     public MovementKeyboardController MovementController;
     public LookAtController LookAtController;
     public LookAtMouse LookAtMouseCtrl;
+    public Rigidbody2D pawn;
 }
