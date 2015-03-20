f3bc4d101d878996b0a0bdbf4a14103f4ee84350
alesegdia <alesegdia@gmail.com>
2015-03-20 22:50:24 +0100

    Added EnemyPool. It can spawn a few enemies at start.

---
+++ project/Assets/Scripts/EnemyPool.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyPool : MonoBehaviour {
+
+    public GameObject prefab;
+    public int maxElements = 30;
+    Queue<GameObject> pool = new Queue<GameObject>();
+    public List<Transform> initialSpawns;
+
+	// Use this for initialization
+	void Start () {
+
+		for( int i = 0; i < maxElements; i++ )
+        {
+            GameObject go = (GameObject)GameObject.Instantiate(((Object)prefab));
+            FollowController fc = go.GetComponent<FollowController>();
+            fc.targetTag = "Player";
+            LookAtController lac = go.GetComponent<LookAtController>();
+            lac.target = GameObject.FindGameObjectWithTag("Player");
+            go.SetActive(false);
+            pool.Enqueue(go);
+        }
+
+		foreach( Transform t in initialSpawns )
+        {
+            Debug.Log("asd");
+            SpawnEnemy(t.transform.position);
+        }
+	}
+
+	public void NotifyDead( Enemy e )
+    {
+        e.isDead = false;
+        e.gameObject.SetActive(false);
+        pool.Enqueue(e.gameObject);
+	}
+
+    GameObject SpawnEnemy( Vector3 position )
+    {
+        GameObject go = pool.Dequeue();
+        go.SetActive(true);
+        go.transform.position = position;
+        return go;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ project/Assets/Scripts/FollowController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowController : MonoBehaviour {
+
+    public string targetTag;
+    public Rigidbody2D pawn;
+    GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag(targetTag);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 vel = target.transform.position - this.transform.position;
+        pawn.velocity = vel.normalized;
+	}
+}
