3f560ceeb9ca45b949124bbed67c6606bc942f02
alesegdia <alesegdia@gmail.com>
2015-03-21 00:53:04 +0100

    Reworked cam follow script.

--- project/Assets/Scripts/FixedFollow.cs
+++ project/Assets/Scripts/FixedFollow.cs
@@ -1,16 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CamController : MonoBehaviour {
+public class FixedFollow : MonoBehaviour {
 
-    GameObject toFollow;
+    public GameObject toFollow;
     Vector3 pos = new Vector3(0, 0);
 
 	// Use this for initialization
 	void Start () {
 
-        toFollow = GameObject.FindGameObjectWithTag("Player");
-	
 	}
 	
 	// Update is called once per frame
