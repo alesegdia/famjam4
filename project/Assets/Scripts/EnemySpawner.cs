57967a4bdc5a0ccaa988d046a3ae6a2995f2b1b4
alesegdia <alesegdia@gmail.com>
2015-03-21 11:43:31 +0100

    Added EnemySpawner. Fixed replenish health on enemies.

---
+++ project/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	EnemyPool enemyPool;
+    public float rateOfSpawn = 1f;
+    float nextSpawn = 0;
+
+	// Use this for initialization
+	void Start () {
+        enemyPool = GameObject.FindObjectOfType<EnemyPool>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Time.time > nextSpawn )
+        {
+            enemyPool.SpawnEnemy( this.transform.position );
+            nextSpawn = Time.time + rateOfSpawn;
+        }
+	}
+}
