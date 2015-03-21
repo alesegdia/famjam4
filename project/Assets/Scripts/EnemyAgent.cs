cecab26aa87d57aa87d25e8db6d230f6ec21ac2f
alesegdia <alesegdia@gmail.com>
2015-03-21 17:06:53 +0100

    Sketching action scene direction. Very basic keyframe framework.

---
+++ project/Assets/Scripts/EnemyAgent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAgent : MonoBehaviour {
+
+    public bool aggresive = false;
+    public FollowController followController;
+    public LookAtController lookAtController;
+    public AlwaysAttack alwaysAttack;
+
+	// Use this for initialization
+	void Start () {
+        followController.enabled = aggresive;
+        lookAtController.enabled = aggresive;
+        alwaysAttack.enabled = aggresive;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        followController.enabled = aggresive;
+        lookAtController.enabled = aggresive;
+        alwaysAttack.enabled = aggresive;
+	}
+}
