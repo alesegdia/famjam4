0968ea938ea8bab40e1967d2033201b71d26b59b
alesegdia <alesegdia@gmail.com>
2015-03-21 03:08:03 +0100

    Added Machete as RaycastMeleeWeapon.

--- project/Assets/Scripts/PlayerShootKeyboardController.cs
+++ project/Assets/Scripts/PlayerShootKeyboardController.cs
@@ -3,15 +3,11 @@ using System.Collections;
 
 public class PlayerShootKeyboardController : MonoBehaviour {
 
-    ProjectileShootWeapon crossbow;
-    RaycastShootWeapon shotgun;
     GameObject crosshair;
     Weapon weaponInUse;
 
 	// Use this for initialization
 	void Start () {
-        crossbow = GameObject.FindGameObjectWithTag("Crossbow").GetComponent<ProjectileShootWeapon>();
-        shotgun = GameObject.FindGameObjectWithTag("Shotgun").GetComponent<RaycastShootWeapon>();
         crosshair = GameObject.FindGameObjectWithTag("Crosshair");
         weaponInUse = GameObject.FindObjectOfType<Player>().WeaponInUse;
 	}
---
+++ project/Assets/Scripts/RaycastMeleeWeapon.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RaycastMeleeWeapon : Weapon {
+
+    public float angle;
+    public LayerMask collisionLayer;
+    public LayerMask damageLayer;
+
+    public float spreadAngle = 1;
+    public int numHits = 1;
+
+    public float rayLimit = Mathf.Infinity;
+    
+    float anglePerBullet;
+    Quaternion quatSpreadAngleFrom;
+    Quaternion quatSpreadAngleTo;
+
+	// Use this for initialization
+	void Start () {
+        anglePerBullet = spreadAngle / numHits;
+        spreadAngle /= 2;
+        quatSpreadAngleFrom = Quaternion.Euler(new Vector3(0, 0, -spreadAngle));
+        quatSpreadAngleTo = Quaternion.Euler(new Vector3(0, 0, spreadAngle));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    Vector3 tmp = new Vector3();
+	List<Collider2D> alreadyHit = new List<Collider2D>();
+    override protected void Shot(Vector2 dir)
+    {
+        RaycastHit2D hit;
+        alreadyHit.Clear();
+        for (int i = 0; i < numHits; i++)
+        {
+			Quaternion angle = Quaternion.Lerp(quatSpreadAngleFrom, quatSpreadAngleTo, 1f/((float)numHits) * ((float)i));
+            tmp = dir;
+            tmp = angle * tmp;
+			Debug.DrawRay(this.transform.position, tmp, Color.green, 5);
+            hit = Physics2D.Raycast(this.transform.position, tmp, rayLimit, collisionLayer);
+            if (hit != null && hit.collider != null && Util.CheckIfLayer(damageLayer.value, hit.collider.gameObject.layer) && !alreadyHit.Contains(hit.collider) )
+            {
+				alreadyHit.Add(hit.collider);
+                Debug.Log(Time.time + ": hit enemy!");
+                hit.collider.gameObject.GetComponent<Health>().currentHealth--;
+            }
+        }
+    }
+}
